9e82884521bcb41114dcc5d41853570f60024828
TOlya <toly.artmchk@gmail.com>
2021-11-24 07:34:08 +0200

    oprimize imports

--- src/main/java/com/example/test_pullrequest_git/JustController.java
+++ src/main/java/com/example/test_pullrequest_git/JustController.java
@@ -1,6 +1,5 @@
 package com.example.test_pullrequest_git;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("qq")
