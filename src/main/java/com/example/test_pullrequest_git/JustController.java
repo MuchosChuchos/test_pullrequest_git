a367f81808daeaa27a892b38fb60c4290b254fd6
TOlya <toly.artmchk@gmail.com>
2021-11-24 07:28:30 +0200

    Create some get tempas

---
+++ src/main/java/com/example/test_pullrequest_git/JustController.java
@@ -0,0 +1,16 @@
+package com.example.test_pullrequest_git;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller("qq")
+public class JustController {
+
+    @GetMapping("/temp")
+    public String getTemp() {
+        return "na temp";
+    }
+
+
+}
