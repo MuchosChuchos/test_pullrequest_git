ed0b5ab34b9fed3ace5e43bd2c7ea36dfb189a0d
TOlya <toly.artmchk@gmail.com>
2021-11-24 07:29:11 +0200

    Remove some imports

--- src/main/java/com/example/test_pullrequest_git/JustController.java
+++ src/main/java/com/example/test_pullrequest_git/JustController.java
@@ -2,7 +2,6 @@ package com.example.test_pullrequest_git;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller("qq")
 public class JustController {
