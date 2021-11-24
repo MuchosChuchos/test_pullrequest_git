c33c5b7fb5fa86d6ec5f45d70e8d63c2df808ee9
TOlya <toly.artmchk@gmail.com>
2021-11-24 07:17:11 +0200

    Initial commit

---
+++ src/main/java/com/example/test_pullrequest_git/TestPullrequestGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_pullrequest_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPullrequestGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestPullrequestGitApplication.class, args);
+    }
+
+}
