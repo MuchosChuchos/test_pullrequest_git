91c9dbebc7f2a5098adaf6f975ee4ba51ef20ec3
TOlya <toly.artmchk@gmail.com>
2021-11-24 12:28:23 +0200

    Optimize imports

--- src/main/java/com/example/test_pullrequest_git/JustController.java
+++ src/main/java/com/example/test_pullrequest_git/JustController.java
@@ -1,9 +1,7 @@
 package com.example.test_pullrequest_git;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
