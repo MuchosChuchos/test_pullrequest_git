4178c327b6ab72e79921b0dee9e805f372cfb1dd
TOlya <toly.artmchk@gmail.com>
2021-11-24 12:15:09 +0200

    Add postSmth

--- src/main/java/com/example/test_pullrequest_git/Mega_temp_CLass.java
+++ src/main/java/com/example/test_pullrequest_git/Mega_temp_CLass.java
@@ -1,4 +1,14 @@
 package com.example.test_pullrequest_git;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.HttpStatusCodeException;
+
 public class Mega_temp_CLass {
+
+    @PostMapping("123")
+    HttpStatus postSmth() {
+        return HttpStatus.OK;
+    }
+
 }
