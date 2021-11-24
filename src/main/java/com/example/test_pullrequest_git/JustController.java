71c42a3446427420e1c0ada81773aba44e452024
TOlya <toly.artmchk@gmail.com>
2021-11-24 07:47:55 +0200

    create delete method

--- src/main/java/com/example/test_pullrequest_git/JustController.java
+++ src/main/java/com/example/test_pullrequest_git/JustController.java
@@ -2,6 +2,7 @@ package com.example.test_pullrequest_git;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,11 @@ public class JustController {
     @GetMapping
     protected ResponseEntity<?> tempasas() {
         return new ResponseEntity<>(HttpStatus.HTTP_VERSION_NOT_SUPPORTED);
+    }
+
+    @DeleteMapping
+    public String gelete() {
+        return "deleted";
     }
 
 }
