1ab92fcd20566bcf00fdd40a522ca5013ea2648b
TOlya <toly.artmchk@gmail.com>
2021-11-24 07:35:55 +0200

    create set tempas

--- src/main/java/com/example/test_pullrequest_git/JustController.java
+++ src/main/java/com/example/test_pullrequest_git/JustController.java
@@ -1,6 +1,7 @@
 package com.example.test_pullrequest_git;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("qq")
@@ -9,6 +10,11 @@ public class JustController {
     @GetMapping("/temp")
     public String getTemp() {
         return "na temp";
+    }
+
+    @PutMapping
+    public String setTempas() {
+        return new String("tempas");
     }
 
 }
