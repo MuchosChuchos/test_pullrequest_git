f5cff1683dd89e407ecdd824efbfd120dda3ad90
TOlya <toly.artmchk@gmail.com>
2021-11-24 08:14:25 +0200

    Add annotation in JavaClasss

--- src/main/java/com/example/test_pullrequest_git/JavaClasss.java
+++ src/main/java/com/example/test_pullrequest_git/JavaClasss.java
@@ -1,4 +1,15 @@
 package com.example.test_pullrequest_git;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class JavaClasss {
+
+
+    @GetMapping
+    String getSir(){
+        return "dfas";
+    }
+
 }
