5dbd802dc020fb02486cc272b600719ead51aa40
anatolii.artemchuk <anatolii.artemchuk@eleks.com>
2022-04-29 18:58:13 +0300

    Delete JavaClass.java

--- src/main/java/com/example/test_pullrequest_git/JavaClass.java
+++
@@ -1,9 +0,0 @@
-package com.example.test_pullrequest_git;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class JavaClass {
-
-}
